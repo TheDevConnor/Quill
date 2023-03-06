4e71dae44e759c5b7f653dec821276a9324490fe
Hypernite <drunkenmonkey4987@gmail.com>
2023-03-05 19:13:07 -0500

    Cleaned up Repo + Added run utils

--- dist/FrontEnd/ast.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// deno-lint-ignore-file no-empty-interface
-//------------------------------------------------------------
-//-----------------       AST TYPES          -----------------
-//--- Define the types of the AST nodes and the AST itself ---
-//------------------------------------------------------------
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ast.js.map
---
+++ math.ql
@@ -0,0 +1,13 @@
+func test (a, b) {
+    return a + b
+}
+
+const test2 := {
+    a: 1,
+    b: 2,
+
+    compute: {
+        A: false
+    },
+}
+
